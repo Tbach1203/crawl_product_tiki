4bd9529fa93a222bfdf5dcc3f8471cfb81ce6ebc
Bách Trần <maitranbach2812@gmail.com>
2025-12-02 17:04:08 +0700

    Them ham lay du lieu tu API

--- crawl_tiki_products.py
+++ crawl_tiki_products.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import requests
+import json
+import time
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36'
+}
 
 def get_Id(df):
     return df['id'].tolist()
@@ -6,13 +13,25 @@ def get_Id(df):
 def product():
     pass
 
-def info_Products():
-    pass
-
+def info_Products(ids):
+    result = []
+    for id in range(len(ids)):
+        response = requests.get(url='https://api.tiki.vn/product-detail/api/v1/products/{}'.format(ids[id]), headers=headers, timeout=10)
+        if response.status_code == 200:
+            result.append(response.json())
+            # time.sleep(2)
+    return result
+        
 if __name__ == "__main__":
     df = pd.read_csv('products-0-200000.csv')
     ids = get_Id(df)
-    print(df[:10])
+    # print(df[:10])
+    products_tiki = info_Products(ids[:10])
+    # print(products_tiki)
+    with open('products.json', 'w', encoding='utf-8') as file:
+        json.dump(products_tiki, file, indent=4)
+    
+
 
     
     
